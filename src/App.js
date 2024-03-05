48f456c8013369018d7da77303d2b2653ab9bbc4
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2024-03-05 15:24:07 +0100

    Added js css and html

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App </h1>
-      <footer>
-        This project is created by{" "}
-        <a
-          href="https://github.com/SoniaCarrilloM"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Sonia Carrillo{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/SoniaCarrilloM/weather-app-three"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather defaultCity="New York" />
+        <footer>
+          This project is created by{" "}
+          <a
+            href="https://github.com/SoniaCarrilloM"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sonia Carrillo{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/SoniaCarrilloM/weather-app-three"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
