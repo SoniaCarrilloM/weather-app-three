bfd4e42d3130d927470e2de2c7489422234908ab
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2024-03-05 14:12:41 +0100

    added links

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is created by Sonia Carrillo and is
+        <a
+          href="https://github.com/SoniaCarrilloM/weather-app-three"
+          target="_blank"
+          ref="noreferrer"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
